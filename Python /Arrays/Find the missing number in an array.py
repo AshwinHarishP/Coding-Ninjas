981012dad312d06f1c4a36c4eed8b76d8a5f1585
Ashwin Harish.P <99186533+AshwinHarishP@users.noreply.github.com>
2023-12-08 22:10:45 +0530

    Create Find the missing number in an array.py

---
+++ Python /Arrays/Find the missing number in an array.py
@@ -0,0 +1,22 @@
+def missingNumber(a, N):
+    xor1 = 0
+    xor2 = 0
+
+    for i in range(N - 1):
+        xor2 = xor2 ^ a[i] 
+    
+    xor1 = xor1 ^ N
+
+    return xor1 ^ xor2
+
+
+def main():
+    N = 5
+    a = [1, 2, 4, 5]
+    ans = missingNumber(a, N)
+    print("The missing number is:", ans)
+
+
+if __name__ == '__main__':
+    main()
+
