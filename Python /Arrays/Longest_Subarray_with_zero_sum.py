e70f34cf0e18e8acce1e98b15496aafd5ffdae63
Ashwin Harish.P <99186533+AshwinHarishP@users.noreply.github.com>
2024-04-24 15:11:09 +0530

    Create Longest_Subarray_with_zero_sum.py

---
+++ Python /Arrays/Longest_Subarray_with_zero_sum.py
@@ -0,0 +1,19 @@
+from typing import *
+
+def getLongestZeroSumSubarrayLength(arr : List[int]) -> int:
+    Sum = maxLen = 0
+    hashmap = dict()
+
+    for i in range(len(arr)):
+        Sum += arr[i]
+
+        if Sum == 0:
+            maxLen = max(maxLen, i+1)
+        
+        elif Sum in hashmap:
+            maxLen = max(maxLen, i - hashmap[Sum])
+        
+        else:
+            hashmap[Sum] = i
+
+    return maxLen
